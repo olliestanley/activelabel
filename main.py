b10fa503a3a55e961968044587d8cd141c9920a0
Oliver Stanley <o.dzineit@gmail.com>
2022-12-04 22:37:39 +0000

    Update

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from pathlib import Path
 
 import pandas as pd
 
-from activelabel.text.classify import TextClassificationLabelJob, TextClassifier
+from activelabel.text.jobs import TextClassificationLabelJob
+from activelabel.text.models import Word2VecSVCTextClassifier
 from activelabel.util import LabelJob, ModelWrapper, infer_mode
 
 
 def get_model(mode: str, label_type: str) -> ModelWrapper:
-    # TODO
-    return TextClassifier(["+", "-", "="])
+    if mode == "text":
+        if label_type == "class":
+            return Word2VecSVCTextClassifier(["+", "-", "="])
+
+    raise ValueError("Unknown mode/label type combination")
 
 
-def get_label_job(mode: str, label_type: str) -> LabelJob:
-    # TODO
+def get_label_job(mode: str, label_type: str, interval: int) -> LabelJob:
     model = get_model(mode, label_type)
-    return TextClassificationLabelJob(model, interval=10)
 
+    if mode == "text":
+        if label_type == "class":
+            return TextClassificationLabelJob(model, interval=interval)
 
-def run(mode: str, label_type: str, source: Path, out: Path, initial: Path):
+    raise ValueError("Unknown mode/label type combination")
+
+def run(mode: str, label_type: str, source: Path, out: Path, initial: Path, interval: int):
     initial_df = pd.read_csv(initial) if initial.exists() else None
-    label_job = get_label_job(mode, label_type)
-    label_df = label_job.start(source, initial_df)
+    label_job = get_label_job(mode, label_type, interval)
+    label_df = label_job.run(source, initial_df)
     label_df.to_csv(out, index=False)
 
 
@@ -29,7 +36,7 @@ def main(args):
     source = Path(args.source)
     mode = infer_mode(source) if args.mode == "infer" else args.mode
     initial_path = Path(args.initial)
-    run(mode, args.label_type, source, Path(args.out), initial_path)
+    run(mode, args.label_type, source, Path(args.out), initial_path, args.interval)
 
 
 if __name__ == "__main__":
@@ -39,5 +46,6 @@ if __name__ == "__main__":
     parser.add_argument("--source", type=str, default="data/raw")
     parser.add_argument("--out", type=str, default="data/output_labels.csv")
     parser.add_argument("--initial", type=str, default="data/input_labels.csv")
+    parser.add_argument("--interval", type=int, default=10)
     args = parser.parse_args()
     main(args)
