f77d123487a3b6ef85bab3fc659dbcecf5754052
Oliver <olivergestanley@gmail.com>
2023-03-02 21:59:18 +0000

    Use yml config

--- frontend/main.py
+++ frontend/main.py
@@ -1,12 +1,22 @@
 import argparse
 from pathlib import Path
+from typing import Any
+import yaml
 
 from activelabel import LabelJob
 from activelabel.text.jobs import TextClassificationLabelJob
 from activelabel.text.models import Word2VecSVCTextClassifier
 import polars as pl
 
-from util import PathLike, infer_mode
+
+class Config:
+    def __init__(self, config: dict[str, Any]):
+        self.mode = config["Mode"]
+        self.label_type = config["Label-Type"]
+        self.source = Path(config["Data-Directory"])
+        self.out = Path(config["Input-Labels"])
+        self.initial = Path(config["Output-Labels"])
+        self.interval = int(config["Retrain-Interval"])
 
 
 def perform_labelling(label_job: LabelJob) -> None:
@@ -47,36 +57,26 @@ def get_job(mode: str, label_type: str, interval: int) -> LabelJob:
     raise NotImplementedError(f"Unavailable mode / label type combination: {mode} / {label_type}")
 
 
-def main(
-    mode: str = "infer",
-    label_type: str = "class",
-    source: PathLike = "data/raw",
-    out: PathLike = "data/output_labels.csv",
-    initial: PathLike = "data/input_labels.csv",
-    interval: int = 10,
-) -> None:
-    source, out, initial = Path(source), Path(out), Path(initial)
-    mode = infer_mode(source) if mode == "infer" else mode
+def main(config_dict: dict[str, Any]) -> None:
+    config = Config(config_dict)
 
-    initial_df = pl.read_csv(initial) if initial.exists() else None
+    initial_df = pl.read_csv(config.initial) if config.initial.exists() else None
 
-    label_job = get_job(mode, label_type, interval)
-    label_job.setup(source, initial_df)
+    label_job = get_job(config.mode, config.label_type, config.interval)
+    label_job.setup(config.source, initial_df)
 
     perform_labelling(label_job)
 
     label_df = pl.from_dict(label_job.labels)
-    label_df.write_csv(out)
+    label_df.write_csv(config.out)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", type=str, default="infer")
-    parser.add_argument("--label-type", type=str, default="class")
-    parser.add_argument("--source", type=str, default="data/raw")
-    parser.add_argument("--out", type=str, default="data/output_labels.csv")
-    parser.add_argument("--initial", type=str, default="data/input_labels.csv")
-    parser.add_argument("--interval", type=int, default=10)
+    parser.add_argument("--config", type=str, default="frontend/config.yml")
     args = parser.parse_args()
 
-    main(args.mode, args.label_type, args.source, args.out, args.initial, args.interval)
+    config_file = Path(args.config)
+    config = yaml.safe_load(config_file.read_text())
+
+    main(config)
